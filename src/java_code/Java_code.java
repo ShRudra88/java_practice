6bfbbb0b75a840431fbff35f2080ddf7131e898d
Md. Sakhawat Hosen <67760305+ShRudra88@users.noreply.github.com>
2023-08-23 00:41:00 +0600

    Update Java_code.java

--- src/java_code/Java_code.java
+++ src/java_code/Java_code.java
@@ -1,21 +1,157 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package java_code;
 
-/**
- *
- * @author IDRIS
- */
 public class Java_code {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       // System.out.print("i am rudra  "); // cmt in java
+        
+       /* System.out.println(3.11);
+        
+        System.out.println(3*11);
+        
+        String name = "Rudra";
+        
+        System.out.println(name);
+        
+        System.out.println("Hello"  + name);
+       
+       Widening Casting (automatically) - converting a smaller type to a larger type size
+byte -> short -> char -> int -> long -> float -> double
+
+Narrowing Casting (manually) - converting a larger type to a smaller size type
+double -> float -> long -> int -> char -> short -> byte
+       
+       
+       
+        
+        
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     
+        System.out.println(isFishTasty);
+
+
+       // upper case // lowercase in java
+       String text = "Sakhawat Rudra";
+       
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+        
+       
+       String fName = "Sakhawat ";
+       String lName = "Rudra";
+       
+        System.out.println(fName.concat(lName));
+        
+        
+       
+         String txt = "We are the so-called \"Vikings\" from the north.";
+         System.out.println(txt);
+         
+         
+       int randomNum = (int)(Math.random() * 101);   // 0 to 100
+        System.out.println(randomNum);
+        
+        
+        int time = 20;
+        if (time < 18) {
+          System.out.println("Good day.");
+        } else {
+          System.out.println("Good evening.");
+        }
+       
+       time = 20;
+       String result = (time < 18) ? "Good day." : "Good evening.";
+       System.out.println(result);
+       
+       
+        int day = 4;
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+        // Outputs "Thursday" (day 4)
+        
+       
+        
+        
+        int day = 4;
+    switch (day) {
+      case 6:
+        System.out.println("Today is Saturday");
+        break;
+      case 7:
+        System.out.println("Today is Sunday");
+        break;
+      default:
+        System.out.println("Looking forward to the Weekend");
+    }
+    // Outputs "Looking forward to the Weekend"
+    
+    
+    // loop
+    int i = 0;
+    while (i < 100) {
+      System.out.println(i);
+      i++;
+    }
+
+    int i = 0;
+    do {
+      System.out.println(i);
+      i++;
+    }
+    while (i < 5);
+
+
+    
+    for (int i = 0; i < 5; i++) {
+    System.out.println(i);
+  }
+     
+       
+       // for each loop in java
+       String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+       for (String i : cars) {
+      System.out.println(i);
+}
+
+       int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+       System.out.println(myNumbers[1][2]); // Outputs 7
+    
+        
+       
+       int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+      for (int i = 0; i < myNumbers.length; ++i) {
+        for(int j = 0; j < myNumbers[i].length; ++j) {
+          System.out.println(myNumbers[i][j]);
+        }
+      }
+       */
+       
+      
     }
     
 }
